1e77dc51e10b0895ab0c306050d6eb4c09f6514b
App Academy Student <student@appacademy.io>
2014-10-08 09:50:45 -0400

    Implement the flash.

---
+++ lib/phase8/flash.rb
@@ -0,0 +1,34 @@
+require 'json'
+require 'webrick'
+
+module Phase8
+  class Flash
+    # find the cookie for this app
+    # deserialize the cookie into a hash
+    attr_reader :now
+
+    def initialize(req)
+      cookie = req.cookies.find do |cookie|
+        cookie.name == '_rails_lite_app_flash'
+      end
+      @now = cookie ? JSON.parse(cookie.value) : Hash.new
+      @next = Hash.new
+    end
+
+
+    def [](key)
+      @now[key]
+    end
+
+    def []=(key, val)
+      @now[key] = val
+      @next[key] = val
+    end
+
+    # serialize the hash into json and save in a cookie
+    # add to the responses cookies
+    def store_flash(res)
+      res.cookies << WEBrick::Cookie.new('_rails_lite_app_flash', @next.to_json)
+    end
+  end
+end
---
+++ spec/p08_flash_spec.rb
@@ -0,0 +1,102 @@
+require 'webrick'
+require 'phase8/flash'
+require 'phase8/controller_base'
+
+describe Phase8::Flash do
+  let(:req) { WEBrick::HTTPRequest.new(Logger: nil) }
+  let(:res) { WEBrick::HTTPResponse.new(HTTPVersion: '1.0') }
+  let(:cook) { WEBrick::Cookie.new('_rails_lite_app_flash', { xyz: 'abc' }.to_json) }
+
+  it "deserializes json cookie if one exists" do
+    req.cookies << cook
+    flash = Phase8::Flash.new(req)
+    flash['xyz'].should == 'abc'
+  end
+
+  describe "#store_flash" do
+    context "without cookies in request" do
+      before(:each) do
+        flash = Phase8::Flash.new(req)
+        flash['first_key'] = 'first_val'
+        flash.store_flash(res)
+      end
+
+      it "adds new cookie with '_rails_lite_app_flash' name to response" do
+        cookie = res.cookies.find { |c| c.name == '_rails_lite_app_flash' }
+        cookie.should_not be_nil
+      end
+
+      it "stores the cookie in json format" do
+        cookie = res.cookies.find { |c| c.name == '_rails_lite_app_flash' }
+        JSON.parse(cookie.value).should be_instance_of(Hash)
+      end
+    end
+
+    context "with cookies in request" do
+      before(:each) do
+        cook = WEBrick::Cookie.new('_rails_lite_app_flash', { pho: "soup" }.to_json)
+        req.cookies << cook
+      end
+
+      it "reads the pre-existing cookie data into hash" do
+        flash = Phase8::Flash.new(req)
+        flash['pho'].should == 'soup'
+      end
+
+      it "saves new but not old data to the cookie" do
+        flash = Phase8::Flash.new(req)
+        flash['machine'] = 'mocha'
+        flash.store_flash(res)
+        cookie = res.cookies.find { |c| c.name == '_rails_lite_app_flash' }
+        h = JSON.parse(cookie.value)
+        h['pho'].should be_nil
+        h['machine'].should == 'mocha'
+      end
+    end
+  end
+end
+
+describe Phase8::ControllerBase do
+  before(:all) do
+    class CatsController < Phase8::ControllerBase
+    end
+  end
+  after(:all) { Object.send(:remove_const, "CatsController") }
+
+  let(:req) { WEBrick::HTTPRequest.new(Logger: nil) }
+  let(:res) { WEBrick::HTTPResponse.new(HTTPVersion: '1.0') }
+  let(:cats_controller) { CatsController.new(req, res) }
+
+  describe "#flash" do
+    it "returns a flash instance" do
+      expect(cats_controller.flash).to be_a(Phase8::Flash)
+    end
+
+    it "returns the same instance on successive invocations" do
+      first_result = cats_controller.flash
+      expect(cats_controller.flash).to be(first_result)
+    end
+  end
+
+  shared_examples_for "storing flash data" do
+    it "should store the flash data" do
+      cats_controller.flash['test_key'] = 'test_value'
+      cats_controller.send(method, *args)
+      cookie = res.cookies.find { |c| c.name == '_rails_lite_app_flash' }
+      h = JSON.parse(cookie.value)
+      expect(h['test_key']).to eq('test_value')
+    end
+  end
+
+  describe "#render_content" do
+    let(:method) { :render_content }
+    let(:args) { ['test', 'text/plain'] }
+    include_examples "storing flash data"
+  end
+
+  describe "#redirect_to" do
+    let(:method) { :redirect_to }
+    let(:args) { ['http://appacademy.io'] }
+    include_examples "storing flash data"
+  end
+end
